27a0b1b6856f34994e92b99c60fbb0914980a995
Valentine Borisevich <valentineborisevich@gmail.com>
2022-12-15 21:32:24 +0300

    HelloUserConsole finished

--- Ex1_HelloUserConsole/Program.cs
+++ Ex1_HelloUserConsole/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Enter username");
+string username = Console.ReadLine();
+Console.WriteLine ("Hello, ");
+Console.WriteLine (username);
