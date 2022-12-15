4ecf8f93c4f9a05bb3af8b053e1845029d9fd422
Valentine Borisevich <valentineborisevich@gmail.com>
2022-12-15 23:44:31 +0300

    cyi added

---
+++ Ex6_Cycle_Image/Program.cs
@@ -0,0 +1,45 @@
+Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 10, ya = 1,
+    xb = 1, yb = 10,
+    xc = 20, yc =20;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+while (count < 10)
+{
+       int what = new Random().Next(0, 3);
+    if(what == 0)
+    {
+    x = (x + xa) / 2;
+    y = (y + ya) / 2;
+    }
+
+    if(what == 1)
+    {
+    x = (x + xb) / 2;
+    y = (x + xb) / 2;
+
+    }
+    if(what == 2)
+    {
+    x = (x + xc) / 2;
+    y = (x + xc) / 2;
+
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count = count++;
+}
